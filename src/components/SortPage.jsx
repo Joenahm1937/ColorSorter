e40b0b18ca2ac10e5b3585a789877f2c0e36a90c
Joseph Nahm <josephnahm1@gmail.com>
2022-01-20 21:07:27 -0800

    3 different screen based on input state

--- src/components/SortPage.jsx
+++ src/components/SortPage.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
+const Alert = () => {
+  return (
+    <>
+      <div className="w-full bg-red-600 flex-1 flex justify-center items-center m-20 text-4xl font-black">
+        Please enter a valid number between 0 and 50
+      </div>
+    </>
+  );
+};
+
 const Bar = ({ color }) => {
   return (
     <div
@@ -9,14 +19,54 @@ const Bar = ({ color }) => {
   );
 };
 
-const SortPage = ({ bars }) => {
+const Video = () => {
+  return (
+    <video autoPlay muted loop className="object-cover">
+      <source
+        src="https://www.youtube.com/watch?v=JrYhY9n3FhQ&ab_channel=AnthonyLopresti"
+        type="video/mp4"
+      />
+    </video>
+  );
+};
+
+const SortPage = ({ bars, invalid }) => {
   return (
     <div className="w-full bg-white flex-1 flex justify-center">
-      {bars.map((bar, i) => (
-        <Bar key={i} color={`rgb(${bar[0]},${bar[1]},${bar[2]})`} />
-      ))}
+      {invalid ? (
+        <Alert />
+      ) : bars.length ? (
+        bars.map((bar, i) => (
+          <Bar key={i} color={`rgb(${bar[0]},${bar[1]},${bar[2]})`} />
+        ))
+      ) : (
+        <Video />
+      )}
     </div>
   );
 };
 
 export default SortPage;
+
+// .video-container {
+//   height: 300px;
+//   width: 300px;
+//   position: relative;
+// }
+
+// .video-container video {
+// width: 100%;
+// height: 100%;
+// position: absolute;
+// object-fit: cover;
+// z-index: 0;
+// }
+
+// /* Just styling the content of the div, the *magic* in the previous rules */
+// .video-container .caption {
+// z-index: 1;
+// position: relative;
+// text-align: center;
+// color: #dc0000;
+// padding: 10px;
+// }
--- src/components/SortTemplate.jsx
+++ src/components/SortTemplate.jsx
@@ -2,9 +2,61 @@ import { useState } from "react";
 import SortPage from "../components/SortPage";
 import { trupleGenerator } from "../helpers/algorithms";
 
+const StartIcon = () => (
+  <svg
+    className="w-12 h-12 bg-blue-400 rounded-full cursor-pointer"
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+    />
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+    />
+  </svg>
+);
+
+const AddIcon = ({ setBars, setSortReady, maxBars, setInvalid }) => (
+  <svg
+    className="w-12 h-12 bg-red-400 rounded-full cursor-pointer"
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    onClick={() => {
+      var num = document.getElementById("numBars").value;
+      if (parseInt(num) && num >= 0 && num <= maxBars) {
+        setInvalid(false);
+        setBars(trupleGenerator(parseInt(num)));
+        setSortReady(true);
+      } else {
+        setInvalid(true);
+      }
+    }}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+    />
+  </svg>
+);
+
 const SortTemplate = ({ algo }) => {
   const maxBars = 100;
   const [bars, setBars] = useState([]);
+  const [sortReady, setSortReady] = useState(false);
+  const [invalid, setInvalid] = useState(false);
   return (
     <div className="h-screen flex flex-col justify-center items-center bg-yellow-300">
       <h1 className="font-black flex justify-center items-center">
@@ -20,36 +72,20 @@ const SortTemplate = ({ algo }) => {
             placeholder={`Max: ${maxBars}`}
           />
         </div>
-        <svg
-          className="w-12 h-12 bg-red-400 rounded-full cursor-pointer"
-          onClick={() => {
-            var num = document.getElementById("numBars").value;
-            if (parseInt(num) && num >= 0 && num <= maxBars) {
-              setBars(trupleGenerator(parseInt(num)));
-            } else {
-              alert("Please Enter a Valid Number");
-            }
-          }}
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
+        <div className="w-20 h-20 flex items-center">
+          {sortReady ? (
+            <StartIcon />
+          ) : (
+            <AddIcon
+              setBars={setBars}
+              setSortReady={setSortReady}
+              maxBars={maxBars}
+              setInvalid={setInvalid}
+            />
+          )}
+        </div>
       </h1>
-      <SortPage bars={bars} />
+      <SortPage bars={bars} invalid={invalid} setInvalid={setInvalid} />
       <div className="h-1/3 font-black flex justify-center items-center">
         <div className="pr-6 bg-yellow-200 m-3">Video Here</div>
         <div className="pr-6 bg-yellow-200 m-3">Description Here</div>
